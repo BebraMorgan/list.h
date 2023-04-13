6c1a2eeb66f9fc2787db1ee51f31ebf680007996
degan <ivanshostik@yandex.ru>
2023-04-14 01:32:22 +0300

    Added first funcs

--- library.h
+++ library.h
@@ -22,34 +22,44 @@ typedef struct SingleLinkedListHead {
 
 slhead_t *sl_head_init(); /* head initiation */
 
-int sl_search_max_id(slhead_t *head); /* max id search */
+int search_max_id(void *head, links); /* max id search */
+
+void list_clear(void *head, int links); /* clear list */
 
 slist_t *slist_append(slhead_t *head, SINGLE_LINKED_LIST_INFO_TYPE *info); /* adding element to the end */
 
 slist_t *slist_get(slhead_t *head, int num); /* getting pointer on the element standing in a given place  */
 
-#endif
+void sl_element_delete_from_tail(slhead_t *head, int num);
 
+void swap(slhead_t *head, slist_t **first, slist_t **second);
+
+#endif
 
 
 #ifdef DOUBLE_LINKED_LIST_INFO_TYPE
 
-typedef struct DoubleLInkedListNode {
-    SINGLE_LINKED_LIST_INFO_TYPE* info;
-    int id;
-    struct DoubleLInkedListNode *prev;
-    struct DoubleLInkedListNode *next;
-}dlist_t;
+        typedef struct DoubleLInkedListNode {
+            SINGLE_LINKED_LIST_INFO_TYPE *info;
+            int id;
+            struct DoubleLInkedListNode *prev;
+            struct DoubleLInkedListNode *next;
+        } dlist_t;
 
-typedef struct DoubleLInkedListHead {
-    int length;
-    dlist_t *first;
-    dlist_t *last;
-}dlhead_t;
+        typedef struct DoubleLInkedListHead {
+            int length;
+            dlist_t *first;
+            dlist_t *last;
+        } dlhead_t;
 
+
+        dlhead_t *dl_head_init();
+
+                dlist_t *dlist_append(dlhead_t *head, DOUBLE_LINKED_LIST_INFO_TYPE*info);
+
+dlist_t *dlist_get(dlhead_t *head, int num);
 
 #endif
-
 
 
 #endif
